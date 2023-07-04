bd2e2c1b29433e35536b10b732249166904108e7
pradeepmondal <mail.pradeepmondal@gmail.com>
2023-07-05 00:57:00 +0530

    issues needed to be fixed with move lift, promise, etc

--- index.js
+++ index.js
@@ -69,10 +69,15 @@ const CreateUI = () => {
 
             lift_space = document.createElement('div');
             lift_space.classList.add('lift_space');
-            lift_space.innerText = `lift space ${i+1}`;
+            // lift_space.innerText = `lift space ${i+1}`;
 
             if(j==0) {
-                lift_space.innerHTML = `<div class="lift_unit" id="#lift+${i}" >Lift ${i+1}</div>`
+                lift_unit = document.createElement('div');
+                lift_unit.classList.add('lift_unit');
+                lift_unit.style.display = 'absolute';
+                lift_unit.innerText = `Lift ${i+1}`;
+                lift_space.appendChild(lift_unit);
+                
                 
             }
 
@@ -86,9 +91,15 @@ const CreateUI = () => {
     }
     //function to create lift doors
     const LiftDoors = () => {
-        for (let i=0; i<lifts; i++) {
-        lift_unit = document.getElementsByClassName('lift_unit')[i];
-        lift_unit.innerHTML = `<div class = "lift_doors"><div class="lift_door_left"></div><div class="lift_door_right"></div></div>`
+        const l_spaces = document.getElementsByClassName('lift_space');
+        for (let i=0; i<l_spaces.length; i++) {
+        const lift_space = l_spaces[i];
+        const door_container = document.createElement('div');
+        door_container.classList.add('door_container');
+        const doors = document.createElement('div');
+        doors.innerHTML = `<div class = "lift_doors"><div class="lift_door_left"></div><div class="lift_door_right"></div></div>`
+        door_container.appendChild(doors);
+        lift_space.appendChild(door_container);
     
     }
     }
@@ -139,7 +150,8 @@ const createLiftsQueue = () => {
     }
 }
 
-const addBtnFunc = () => {
+// function to add button functionality
+const addBtnFunc =  () => {
     const floors = localStorage.getItem('floors');
     const lifts = localStorage.getItem('lifts');
 
@@ -152,37 +164,130 @@ const addBtnFunc = () => {
         floor_up.addEventListener('click', () => {
             
         })
+
         floor_down.addEventListener('click', () => {
             
-    })
-        lift_up.addEventListener('click', () => {
+})
+
+        const LiftUpFunc = async (i=i) => {
             curFloor = JSON.parse(localStorage.getItem(`lift${j+1}CurFloor`).trim());
+            let isMove = true;
                 if ((curFloor[0] === i ) && (curFloor[1] >= 0)) {
+                    OpenDoors(j);    
+                }
+
+                else if (curFloor[0] < i ){
+                
+                    console.log(`lift is inside func`)
+                    const liftQueue =  JSON.parse(localStorage.getItem(`lift${j+1}Queue`));
+                    
+
+                    const t = i
+
+                    if (!(liftQueue.up.includes(t))) {
+
+                        liftQueue.up.push(t);
+                        console.log(liftQueue.up)
+                        localStorage.setItem(`lift${j+1}Queue`, JSON.stringify(liftQueue))
+                    
+                
+                        // Call Move Lift Function
+                        if(isMove) {
+                            liftQueue.up.pop(t);
+                            localStorage.setItem(`lift${j+1}Queue`, JSON.stringify(liftQueue));
+                            await new Promise((resolve, reject) => { MoveLift(j, i-curFloor[0]);
+                                setTimeout(() => {
+                                    console.log(`lift reached floor ${Math.abs(i)}`);
+                                    localStorage.setItem(`lift${j+1}CurFloor`, JSON.stringify([i, 1]));
+                                    console.log(`Now Current floor is  ${Math.abs(i)}`);
+                                    resolve();
+                                }, 5000+Math.abs(curFloor[0]-i)*2000);} );
+
+                                
+                            const liftQueue1 =  JSON.parse(localStorage.getItem(`lift${j+1}Queue`));
+                            console.log(`Current Queue is   ${liftQueue1.up}`);
+                            if(liftQueue1.up.length !== 0) {
+                                const arg = (liftQueue1.up.filter((item) => item >= i).sort())[0];
+                            console.log(`Next in Queue is   ${arg}`);
+                                LiftUpFunc(arg);
+                                
+                            }
+
+
+
+                        }
+                    }
+                }
+}
+
+        lift_up.addEventListener('click', () => {LiftUpFunc(i)} )
+
+        lift_down.addEventListener('click', () => {
+            curFloor = JSON.parse(localStorage.getItem(`lift${j+1}CurFloor`).trim());
+            let isMove = false;
+                if ((curFloor[0] === i ) && (curFloor[1] <= 0)) {
                 OpenDoors(j);    
                 }
                 else {
                 const liftQueue =  JSON.parse(localStorage.getItem(`lift${j+1}Queue`));
+                if(liftQueue.down.length === 0) {
+                    isMove = true;
+                }
+
                 const t = i
-                if (!(liftQueue.up.includes(t))) {
-                liftQueue.up.push(t);
-                console.log(liftQueue.up)
+                if (!(liftQueue.down.includes(t))) {
+                liftQueue.down.push(t);
+                console.log(liftQueue.down)
                 localStorage.setItem(`lift${j+1}Queue`, JSON.stringify(liftQueue))
                 
                 // Call Move Lift Function
-
+                if(isMove) {
+                    MoveLift(j, -i);
                 }
-                
-                }
-    })
-
-
-}
-
+        }
     }
 
 
 
+        })
+
+
+
 }
+}
+}
+
+
+
+// create a function to move the lift
+const MoveLift = 
+    (lift, k) => {
+
+        function getOffset(el) {
+            const rect = el.getBoundingClientRect();
+            return {
+              left: rect.left + window.scrollX,
+              top: rect.top 
+            };
+          }
+        
+        const lifts = localStorage.getItem('lifts');  
+        const curFloor = JSON.parse(localStorage.getItem(`lift${lift+1}CurFloor`).trim());
+        const liftQueue =  JSON.parse(localStorage.getItem(`lift${lift+1}Queue`));
+        const lift_unit = document.getElementsByClassName('lift_unit')[lift];
+        const lift_space_g = document.getElementsByClassName('floor')[0].getElementsByClassName('lift_space')[0];
+        const lift_space_f = document.getElementsByClassName('floor')[1].getElementsByClassName('lift_space')[0];
+        const lift_space_diff = getOffset(lift_space_f).top - getOffset(lift_space_g).top;
+        console.log(getOffset(lift_space_f).top, getOffset(lift_space_g).top ,lift_space_diff);
+    
+        lift_unit.style.transition = `all ${Math.abs(curFloor[0]-k)*2}s `;
+    
+        lift_unit.style.transform = `translate(0px, ${ (k*lift_space_diff)}px)`;
+        setTimeout(() => {OpenDoors((k*lifts)+lift);}, Math.abs(curFloor[0]-k)*2000);
+        
+        
+       
+    }
 
 
 
@@ -210,7 +315,7 @@ const Intro = () => {
 
 
 
-
+// Starting the Intro part
 Intro();
 
 
