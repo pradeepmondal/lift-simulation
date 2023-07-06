42f81865534409a303e3efd5cf845a17f0d59845
pradeepmondal <mail.pradeepmondal@gmail.com>
2023-07-06 05:32:15 +0530

    task completed only ui enhancement needed

--- index.js
+++ index.js
@@ -162,28 +162,35 @@ const addBtnFunc =  () => {
         const lift_up = document.getElementsByClassName('lift_up')[(i*lifts)+j];
         const lift_down = document.getElementsByClassName('lift_down')[(i*lifts)+j];
         floor_up.addEventListener('click', () => {
+            for (let l=0; l<lifts; l++) {
+                LiftFunc(i, 1, l);
+            }
             
         })
 
         floor_down.addEventListener('click', () => {
+            for (let l=0; l<lifts; l++) {
+                LiftFunc(i+1, -1, l);
+            }
             
 })
 
-        const LiftFunc = async (floorNum, dir) => {
+        const LiftFunc = async (floorNum, dir, j=j) => {
             let curFloor = JSON.parse(localStorage.getItem(`lift${j+1}CurFloor`).trim());
             let isMove = false;
 
-            if ((curFloor[0] === floorNum) && (curFloor[1] >= 0)) {
-                isMove = true;
+            if(dir === 1) {
+                if ((curFloor[0] === floorNum) && (curFloor[1] >= 0)) {
+                
+                console.log(`lift is on same floor, opening the doors`);
+                // MoveLift(j, floorNum, dir);
+                
             }
                 
-
-                
-                
-                    console.log(`lift is on different floor, adding to queue`)
+                    console.log(`lift is on different floor, adding to queue`);
                     const liftQueue =  JSON.parse(localStorage.getItem(`lift${j+1}Queue`));
 
-                    if(liftQueue.up.length === 0) {
+                    if((liftQueue.up.length === 0)&&(liftQueue.down.length === 0)) {
                         console.log(`Queue is empty, adding to queue, and moving lift`)
                         isMove = true;}
                     
@@ -193,55 +200,80 @@ const addBtnFunc =  () => {
                     if (!(liftQueue.up.includes(t))) {
 
                         liftQueue.up.push(t);
-                        console.log(`New Queue is ${liftQueue.up}`);
+                        console.log(`New Up Queue for lift${j+1}  is ${liftQueue.up}`);
                         localStorage.setItem(`lift${j+1}Queue`, JSON.stringify(liftQueue));
-                        console.log(`lift queue is ${liftQueue.up}`);
+                        console.log(`lift ${j+1} up queue is ${liftQueue.up}`);
                         // console.log(`lift ${j+1} priority  is ${preferDictGenUp(liftQueue.up, curFloor[0])[0]}`);
                     
                 
                         // Call Move Lift Function
                         if(isMove) {
-                            MoveLift(j, i);
+                            MoveLift(j, floorNum, dir);
                                 
                             }
 
 
 
                         }
+
+
+                
+            }
+             else if(dir === -1) {
+                if ((curFloor[0] === floorNum) && (curFloor[1] <= 0)) {
+                
+                console.log(`lift is on same floor, opening the doors`)
+                MoveLift(j, floorNum, dir);
+
+            }
+            
+                    console.log(`lift is on different floor, adding to queue`);
+                    const liftQueue =  JSON.parse(localStorage.getItem(`lift${j+1}Queue`));
+
+                    if((liftQueue.up.length === 0)&&(liftQueue.down.length === 0)) {
+                        console.log(`Queue is empty, adding to queue, and moving lift`)
+                        isMove = true;}
+                    
+
+                    const t = floorNum
+
+                    if (!(liftQueue.down.includes(t))) {
+
+                        liftQueue.down.push(t);
+                        console.log(`New Down Queue for lift${j+1}  is ${liftQueue.down}`);
+                        localStorage.setItem(`lift${j+1}Queue`, JSON.stringify(liftQueue));
+                        console.log(`lift ${j+1} down queue is ${liftQueue.down}`);
+                        // console.log(`lift ${j+1} priority  is ${preferDictGenUp(liftQueue.up, curFloor[0])[0]}`);
+                    
+                
+                        // Call Move Lift Function
+                        if(isMove) {
+                            MoveLift(j, floorNum, dir);
+                                
+                            }
+
+
+
+                        }
+
+
+                
+        }
+
+            
+                
+
+                
+                
+                    
+                    
                     
                 }
 
 
-        lift_up.addEventListener('click', () => {LiftFunc(i, 1)} )
+        lift_up.addEventListener('click', () => {LiftFunc(i, 1, j)} )
 
-        lift_down.addEventListener('click', () => {
-            curFloor = JSON.parse(localStorage.getItem(`lift${j+1}CurFloor`).trim());
-            let isMove = false;
-                if ((curFloor[0] === i ) && (curFloor[1] <= 0)) {
-                OpenDoors(j);    
-                }
-                else {
-                const liftQueue =  JSON.parse(localStorage.getItem(`lift${j+1}Queue`));
-                if(liftQueue.down.length === 0) {
-                    isMove = true;
-                }
-
-                const t = i
-                if (!(liftQueue.down.includes(t))) {
-                liftQueue.down.push(t);
-                console.log(liftQueue.down)
-                localStorage.setItem(`lift${j+1}Queue`, JSON.stringify(liftQueue))
-                
-                // Call Move Lift Function
-                if(isMove) {
-                    MoveLift(j, -i);
-                }
-        }
-    }
-
-
-
-        })
+        lift_down.addEventListener('click', () => {LiftFunc(i+1, -1, j)} )
 
 
 
@@ -252,7 +284,9 @@ const addBtnFunc =  () => {
 
 
 // create a function to move the lift 
-    async function MoveLift (lift, k) {
+    async function MoveLift (lift, k, dir) {
+        let con = false;
+        let con2 = false; 
 
         function getOffset(el) {
             const rect = el.getBoundingClientRect();
@@ -261,9 +295,11 @@ const addBtnFunc =  () => {
               top: rect.top 
             };
           }
-        
-        
 
+          let dir_ = 0;
+        
+        
+        if(dir === 1) {
         await new Promise((resolve, reject) => {
 
             const lifts = localStorage.getItem('lifts');  
@@ -273,6 +309,9 @@ const addBtnFunc =  () => {
             if ((curFloor[0] === k ) && (curFloor[1] >= 0)) {
                 console.log(`lift is on same floor, opening doors`)
                 OpenDoors((k*lifts)+lift);
+                dir_ = 1;
+                con = true;
+                
                 setTimeout(() => {resolve();}, 7500);     
             }
             else {
@@ -297,9 +336,9 @@ const addBtnFunc =  () => {
 
         let temp = curFloor[0];
 
-        const dir = (curFloor[0]<k) ? 1 : -1;
-        curFloor[0], curFloor[1] = k, dir;
-        localStorage.setItem(`lift${lift+1}CurFloor`, JSON.stringify([k, dir]));
+         dir_ = (curFloor[0]<k) ? 1 : -1;
+        curFloor[0], curFloor[1] = k, dir_;
+        localStorage.setItem(`lift${lift+1}CurFloor`, JSON.stringify([k, dir_]));
 
 
         setTimeout(() => {OpenDoors((k*lifts)+lift);console.log(`lift ${lift+1} reached to ${k}`);
@@ -333,15 +372,147 @@ const addBtnFunc =  () => {
     }
         
         });
+        if(con) {
+        let liftQueue =  JSON.parse(localStorage.getItem(`lift${lift+1}Queue`));
+        const idx = liftQueue.up.indexOf(k);
+        liftQueue.up.splice(idx, 1);
+        localStorage.setItem(`lift${lift+1}Queue`, JSON.stringify(liftQueue));}
+
+    }
+    else if(dir === -1) {
+        await new Promise((resolve, reject) => {
+
+            const lifts = localStorage.getItem('lifts');  
+            let curFloor = JSON.parse(localStorage.getItem(`lift${lift+1}CurFloor`).trim());
+            let liftQueue =  JSON.parse(localStorage.getItem(`lift${lift+1}Queue`));
+
+            if ((curFloor[0] === k ) && (curFloor[1] <= 0)) {
+                console.log(`lift is on same floor, opening doors`)
+                OpenDoors((k*lifts)+lift);
+                dir_ = -1;
+                con2 = true;
+                
+                setTimeout(() => {resolve();}, 7500);
+
+            }
+            else {
+
+            
+            let lift_unit = document.getElementsByClassName('lift_unit')[lift];
+            const lift_space_g = document.getElementsByClassName('floor')[0].getElementsByClassName('lift_space')[0];
+            const lift_space_f = document.getElementsByClassName('floor')[1].getElementsByClassName('lift_space')[0];
+            const lift_space_diff = getOffset(lift_space_f).top - getOffset(lift_space_g).top;
+
+        console.log(getOffset(lift_space_f).top, getOffset(lift_space_g).top ,lift_space_diff);
+    
+        lift_unit.style.transition = `all ${Math.abs(curFloor[0]-k)*2}s `;
+
+        const diff = (k-curFloor[0]);
+
+        console.log(`current position of lift is ${getOffset(lift_unit).top}`)
+
+        console.log(`value of next floor is ${k} and value of current floor is ${curFloor[0]} lift would move ${ getOffset(lift_unit).top + ((k-curFloor[0])*lift_space_diff)}px `)
+    
+        lift_unit.style.transform += `translate(0px, ${ ((k-curFloor[0])*lift_space_diff)}px)`;
+
+        let temp = curFloor[0];
+
+        dir_ = (curFloor[0]<k) ? 1 : -1;
+        curFloor[0], curFloor[1] = k, dir_;
+        localStorage.setItem(`lift${lift+1}CurFloor`, JSON.stringify([k, dir_]));
+
+
+        setTimeout(() => {OpenDoors((k*lifts)+lift);console.log(`lift ${lift+1} reached to ${k}`);
+    
+    
+        
+        
+        
+   
+        setTimeout(() => {
+            liftQueue =  JSON.parse(localStorage.getItem(`lift${lift+1}Queue`));
+            const idx = liftQueue.down.indexOf(k);
+            liftQueue.down.splice(idx, 1);
+            localStorage.setItem(`lift${lift+1}Queue`, JSON.stringify(liftQueue));
+
+            resolve(); console.log('Resolved Down');}, 7500); 
+                
+    }, Math.abs(temp-k)*2000);
+
+        for (let i=0; i<Math.abs(temp-k)-1; i++) {
+            setTimeout(() => {temp += (diff/Math.abs(diff)); console.log(`lift ${lift+1} is at ${temp}`);}, (i+1)*2000);
+        }
+      
+            
+        setTimeout(() => {console.log(`lift ${lift+1} is at ${temp+(diff/Math.abs(diff))}`)}, Math.abs(diff)*2000);    
+            console.log(temp);
+        //     localStorage.setItem(`lift${lift+1}CurFloor`, JSON.stringify(curFloor));
+        //     console.log(`lift ${lift+1} is at ${curFloor[0]}`);
+        //     // setTimeout( () => {}, 2000);
+        
+    }
+        
+        });
+        if(con2) {
+            let liftQueue =  JSON.parse(localStorage.getItem(`lift${lift+1}Queue`));
+            const idx = liftQueue.down.indexOf(k);
+            liftQueue.down.splice(idx, 1);
+            localStorage.setItem(`lift${lift+1}Queue`, JSON.stringify(liftQueue));
+
+        }
+
+    }
 
         
-        
+        console.log(`current lift direction is ${dir_}`);
         let liftQueue =  JSON.parse(localStorage.getItem(`lift${lift+1}Queue`));
         let curFloor = JSON.parse(localStorage.getItem(`lift${lift+1}CurFloor`).trim());
-        if (liftQueue.up.length > 0) {
+        if (!((liftQueue.up.length === 0)&&(liftQueue.down.length === 0))) {
+            if ((dir_ === 1) && (liftQueue.up.length === 0)) {
+                dir_ = -1;
+                localStorage.setItem(`lift${lift+1}CurFloor`, JSON.stringify([curFloor[0], dir_]));
+            }
+            else if ((dir_ === 1) && ((liftQueue.up.length !== 0) && Math.max(liftQueue.up)<curFloor[0])) {
+                dir_ = -1;
+                localStorage.setItem(`lift${lift+1}CurFloor`, JSON.stringify([curFloor[0], dir_]));
+            }
+            else if ((dir_ === -1) && (liftQueue.down.length === 0)) {
+                dir_ = 1;
+                localStorage.setItem(`lift${lift+1}CurFloor`, JSON.stringify([curFloor[0], dir_]));
+            }
+            else if ((dir_ === -1) && ((liftQueue.down.length !== 0) && Math.min(liftQueue.down)>curFloor[0])) {
+                dir_ = 1;
+                localStorage.setItem(`lift${lift+1}CurFloor`, JSON.stringify([curFloor[0], dir_]));
+            }
+            if(dir_ === 1) {
+                if (liftQueue.up.length !== 0) {
             const preferDict = preferDictGenUp(liftQueue.up, curFloor[0]);
+            console.log(`after conditions, current lift direction is ${dir_}`);
             console.log(`Next floor is ${preferDict[0]}`);
-            MoveLift(lift, preferDict[0]);
+            MoveLift(lift, preferDict[0], 1);}
+            else {
+                const preferDict = preferDictGenDown(liftQueue.down, curFloor[0]);
+                console.log(`after conditions, current lift direction is ${dir_}`);
+                console.log(`Next floor is ${preferDict[0]}`);
+                MoveLift(lift, preferDict[0], -1 );
+
+            }
+        }
+            else if(dir_ === -1) {
+                if (liftQueue.down.length !== 0) {
+                const preferDict = preferDictGenDown(liftQueue.down, curFloor[0]);
+                console.log(`after conditions, current lift direction is ${dir_}`);
+                console.log(`Next floor is ${preferDict[0]}`);
+                MoveLift(lift, preferDict[0], -1 );}
+                else {
+
+                const preferDict = preferDictGenUp(liftQueue.up, curFloor[0]);
+                console.log(`after conditions, current lift direction is ${dir_}`);
+                console.log(`Next floor is ${preferDict[0]}`);
+                MoveLift(lift, preferDict[0], 1 );
+
+                }
+            }
         }
         
         
@@ -352,8 +523,19 @@ const addBtnFunc =  () => {
         const preferDict = {};
             let key = 0;
             list.sort();
-            list.forEach((item) => {if(item>k)  {preferDict[key] = item; key++;} });
+            list.forEach((item) => {if(item>=k)  {preferDict[key] = item; key++;} });
             list.forEach((item) => {if(item<k)  {preferDict[key] = item; key++;} });
+
+            return preferDict;
+    }
+
+    const preferDictGenDown = (list, k) => {
+        const preferDict = {};
+            let key = 0;
+            list.sort();
+            list.reverse();
+            list.forEach((item) => {if(item<=k)  {preferDict[key] = item; key++;} });
+            list.forEach((item) => {if(item>k)  {preferDict[key] = item; key++;} });
 
             return preferDict;
     }
